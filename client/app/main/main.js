3435f376aa517a143e0a4008f236772e3800f49a
adamelevate <adamelevate@gmail.com>
2015-09-25 16:07:26 -0500

    init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('hintTestApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
